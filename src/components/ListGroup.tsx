0124f07f4f274de82edb64fe8e03eb779228b07a
Axel Olivecrona <112471629+axxcident@users.noreply.github.com>
2023-03-19 11:28:46 +0100

    33:00-38:00 conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,28 @@
 import { Fragment } from "react";
 
 function ListGroup() {
+  let items = ["New York", "Stockholm", "Tokyo", "Paris", "Oslo"];
+  items = [];
+
+  // items = [];
+  // if (items.length === 0) {
+  //   return (
+  //     <>
+  //       <h1>List</h1>
+  //       <p>No list found</p>
+  //     </>
+  //   );
+  // }
+
   return (
     <Fragment>
       {/* kan också strunta i att ha Fragment och bara skriva "<>". behöver inte ens import nu */}
       <h1>List</h1>
+      {items.length === 0 && <p>No list found</p>}
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item, key) => (
+          <li key={key}>{item}</li>
+        ))}
       </ul>
     </Fragment>
   );
