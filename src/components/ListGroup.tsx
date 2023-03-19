cd9bea02e22add8abf44c8e8b163dc8c01a91875
Axel Olivecrona <112471629+axxcident@users.noreply.github.com>
2023-03-19 12:32:10 +0100

    Handling Events

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,27 +1,22 @@
 import { Fragment } from "react";
+import { MouseEvent } from "react";
 
 function ListGroup() {
   let items = ["New York", "Stockholm", "Tokyo", "Paris", "Oslo"];
-  items = [];
 
-  // items = [];
-  // if (items.length === 0) {
-  //   return (
-  //     <>
-  //       <h1>List</h1>
-  //       <p>No list found</p>
-  //     </>
-  //   );
-  // }
+  // Event handler
+  const handleClick = (event: MouseEvent) =>
+    console.log(event.target.outerText, event);
 
   return (
     <Fragment>
-      {/* kan också strunta i att ha Fragment och bara skriva "<>". behöver inte ens import nu */}
       <h1>List</h1>
       {items.length === 0 && <p>No list found</p>}
       <ul className="list-group">
         {items.map((item, key) => (
-          <li key={key}>{item}</li>
+          <li className="list-group-item" key={key} onClick={handleClick}>
+            {item}
+          </li>
         ))}
       </ul>
     </Fragment>
